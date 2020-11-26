ce427a9ce7ebc7929f7b0de542eecff0b5754601
MrFengHa <1109179567@qq.com>
2020-11-26 15:16:29 +0800

    springAOP cglib动态代理

---
+++ spring_aop/src/main/java/com/home/proxy/cglib/Advice.java
@@ -0,0 +1,19 @@
+package com.home.proxy.cglib;
+
+/**
+ * 文件描述
+ *
+ * @Author 冯根源
+ * @create 2020/11/26 14:38
+ */
+public class Advice {
+    public void before(){
+        System.out.println("前置增强");
+    }
+
+    public void after(){
+        System.out.println("后置增强");
+    }
+
+
+}
---
+++ spring_aop/src/main/java/com/home/proxy/cglib/Target.java
@@ -0,0 +1,13 @@
+package com.home.proxy.cglib;
+
+/**
+ * 文件描述
+ *
+ * @Author 冯根源
+ * @create 2020/11/26 15:07
+ */
+public class Target {
+    public void save(){
+        System.out.println("save running.....");
+    }
+}
