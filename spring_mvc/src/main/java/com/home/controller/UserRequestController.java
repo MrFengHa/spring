7108e430c37d50b6960ded016b4052f349dbb76f
MrFengHa <1109179567@qq.com>
2020-11-30 18:12:57 +0800

    @RequestParam

--- spring_mvc/src/main/java/com/home/controller/UserRequestController.java
+++ spring_mvc/src/main/java/com/home/controller/UserRequestController.java
@@ -5,6 +5,7 @@ import com.home.domain.VO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -18,7 +19,11 @@ import java.util.List;
 @Controller
 
 public class UserRequestController {
-
+    @ResponseBody
+    @RequestMapping("quick16")
+    public void save06(@RequestParam("name") String username){
+        System.out.println(username);
+    }
 
     @ResponseBody
     @RequestMapping("quick15")
