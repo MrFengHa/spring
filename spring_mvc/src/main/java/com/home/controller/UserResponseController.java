f220e0871c3f4f7bd4db0f7809a99125de0e9813
MrFengHa <1109179567@qq.com>
2020-11-30 16:13:30 +0800

    springMVC的数据响应

--- spring_mvc/src/main/java/com/home/controller/UserResponseController.java
+++ spring_mvc/src/main/java/com/home/controller/UserResponseController.java
@@ -10,10 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 /**
  * 文件描述
