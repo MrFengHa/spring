bb40e58befc9afeff312ab6ca1da48a5bf532965
MrFengHa <1109179567@qq.com>
2021-01-10 22:08:16 +0800

    自动登录

--- springsecuritytest/src/main/java/com/home/config/SecurityConfig.java
+++ springsecuritytest/src/main/java/com/home/config/SecurityConfig.java
@@ -9,6 +9,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
 
 /**
  * 文件描述
@@ -22,10 +26,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsService userDetailsService;
 
+    /**
+     * 注入数据源
+     */
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository() {
+        JdbcTokenRepositoryImpl jdbcTokenRepository = new JdbcTokenRepositoryImpl();
+        jdbcTokenRepository.setDataSource(dataSource);
+        //设置的时候把表创建出来
+        //jdbcTokenRepository.setCreateTableOnStartup(true);
+        return jdbcTokenRepository;
+    }
+
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(getPasswordEncoder());
     }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -54,6 +75,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 //设置没有访问权限的页面
                 .and().exceptionHandling().accessDeniedPage("/unauth.html")
+
+                //开启自动登录
+                .and().rememberMe().tokenRepository(persistentTokenRepository())
+                //设置有效时长单位秒
+                .tokenValiditySeconds(60)
+                .userDetailsService(userDetailsService)
+
                 //关闭csrf防护
                 .and().csrf().disable();
     }
