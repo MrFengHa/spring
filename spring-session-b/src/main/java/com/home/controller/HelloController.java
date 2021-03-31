3cb58f89cca2ed7a3b7cea6a86b406b84bdabf7e
MrFengHa <1109179567@qq.com>
2021-03-31 17:04:10 +0800

    SoringSession

---
+++ spring-session-b/src/main/java/com/home/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.home.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * 文件描述
+ *
+ * @Author 冯根源
+ * @create 2021/3/31 16:10
+ */
+@RestController
+public class HelloController {
+    @RequestMapping("/test/spring/session/retrieve")
+    public String testSession(HttpSession session){
+        String value = (String) session.getAttribute("king");
+        return value;
+    }
+}
